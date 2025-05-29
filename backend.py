460bac188d00bb7f2e372ec94e89e2b2a07d5220
Tanu Agarwal <116576884+tanu100894@users.noreply.github.com>
2025-05-29 06:22:07 +0200

    Get a temperature/Sky data from the weather API and plotting it dynamically.

--- backend.py
+++ backend.py
@@ -7,18 +7,14 @@ load_dotenv()
 # Retrieve API key from environment variables
 api_key = os.getenv("api_key")
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days):
     url = (f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}")
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", forecast_days=3))
